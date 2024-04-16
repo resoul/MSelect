4ccfa954e7c7bf8e39877ddd739c2d6160a15e4e
resoul <resoul.ua@icloud.com>
2024-04-17 02:33:10 +0300

    add macOS project

---
+++ macOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macOS
+//
+//  Created by resoul on 17.04.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
